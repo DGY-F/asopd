1da07366eb888998fb9c5bcfd266985226cc280c
DGY-F <alfathirapridiafaris@gmail.com>
2024-10-02 11:58:57 +0700

    Add files via upload

---
+++ terakhir.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int i,n = 0, jumlah = 0;
+	
+	cout<< " masukan jumalah :";
+	cin>> i;
+	jumlah = jumlah+i;
+	cout << i << " - ";
+	i-=2;
+	while (1<n)
+		{
+		if(i%2 != 0)
+	}
+	cout << i << " - ";
+	jumlah = jumlah-i;
+	i--;	
+}
+cout << " " << i << " - ";
+jumlah - jumlah-i;
+i-=2;
